104de9e4a3960ca7397c7e7d5521785d355c6622
Ahmed Hisham Saad Othman <59310650+ahmed0thman@users.noreply.github.com>
2024-05-11 05:38:44 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const cards = Array.from(document.querySelectorAll('.card'));
+const cardsContainer = document.getElementById('cards');
+
+cardsContainer.addEventListener('mousemove', (e) =>{
+  for(const card of cards){
+    rect = card.getBoundingClientRect()
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
+    card.style.setProperty('--mouse-x', `${x}px`);
+    card.style.setProperty('--mouse-y', `${y}px`);
+  }
+});
